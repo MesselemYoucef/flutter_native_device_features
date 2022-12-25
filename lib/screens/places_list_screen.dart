ca5fc45458f66556beceeb008765b66a6bdd042e
Youcef <messelemy@gmail.com>
2022-12-25 13:14:14 +0400

    created an add image screen with image input widget

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -6,16 +7,19 @@ class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Places"),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: const Center(child: CircularProgressIndicator(),)
-    );
+        appBar: AppBar(
+          title: const Text("Your Places"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: const Center(
+          child: CircularProgressIndicator(),
+        ));
   }
 }
